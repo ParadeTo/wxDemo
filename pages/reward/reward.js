4556e5fdc6a82c98d3cfa2477b6a8b2c9c353e88
youxingzhi@qq.com <woshiyxz00>
2016-12-15 17:28:30 +0800

    微信小程序

--- pages/reward/reward.js
+++ pages/reward/reward.js
@@ -1,6 +1,9 @@
 // pages/reward/reward.js
 Page({
-  data:{},
+  data:{
+    activeArr:[],
+    activeIndex:0
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
@@ -15,5 +18,33 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  reward:function(){
+    var that = this;
+    var i = 0;
+    for(i =0;i<20;i++) {
+      setTimeout(300);
+    }
+    // this.timeout(300);
+  },
+  timeout: function(ms) {
+    var that = this;
+    setTimeout(()=>{
+      var tmp = that.data.activeArr.slice(0);
+      var _activeIndex = this.data.activeIndex;
+      if(tmp.length === 0) {
+        tmp[0] = "active";
+        _activeIndex = 0;
+      } else {
+        tmp[_activeIndex++] = "";
+        _activeIndex = _activeIndex % 12;
+        tmp[_activeIndex] = "active";
+      }
+      that.setData({
+        activeArr: tmp,
+        activeIndex: _activeIndex
+      });
+      console.log(tmp);
+    },ms)
   }
 })
